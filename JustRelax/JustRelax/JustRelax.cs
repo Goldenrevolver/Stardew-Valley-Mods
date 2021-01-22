3238a063aaf55fee6ac3cc4ec5d7cc82b9e67b1d
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2021-01-22 17:54:58 +0100

    New Mod: Just Relax, version 1.0

---
+++ JustRelax/JustRelax/JustRelax.cs
@@ -0,0 +1,42 @@
+﻿namespace JustRelax
+{
+    using StardewModdingAPI;
+    using StardewModdingAPI.Events;
+    using StardewValley;
+
+    public class JustRelax : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Helper.Events.GameLoop.UpdateTicked += RelaxHeal;
+        }
+
+        private void RelaxHeal(object sender, UpdateTickedEventArgs args)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+
+            // if we are in bed and in singleplayer (because multiplayer already has healing) or we are sitting
+            if ((Game1.player.isInBed && !Game1.IsMultiplayer) || Game1.player.isSitting)
+            {
+                // if the game is not paused in singleplayer and half a second passed (same regen rate as bed in multiplayer)
+                if (!(Game1.player.hasMenuOpen && !Game1.IsMultiplayer) && !Game1.paused && args.Ticks % 30 == 0)
+                {
+                    // upper case stamina property has an inbuilt check that it can't go over maxStamina so we can simply add one
+                    if (Game1.player.Stamina < Game1.player.maxStamina)
+                    {
+                        Game1.player.Stamina++;
+                    }
+
+                    // health is an int so we can simply add one
+                    if (Game1.player.health < Game1.player.maxHealth)
+                    {
+                        Game1.player.health++;
+                    }
+                }
+            }
+        }
+    }
+}
