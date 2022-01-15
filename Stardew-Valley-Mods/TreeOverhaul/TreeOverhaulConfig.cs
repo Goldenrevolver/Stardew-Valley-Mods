fec006d5cc0e25c68feeee8c72505595364f9b60
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2022-01-15 16:35:03 +0100

    Tree Overhaul Mushroom tappers produce in winter, refactoring

--- Stardew-Valley-Mods/TreeOverhaul/TreeOverhaulConfig.cs
+++ Stardew-Valley-Mods/TreeOverhaul/TreeOverhaulConfig.cs
@@ -144,15 +144,9 @@
 
         private static int GetIndexFromArrayElement(string[] options, string element)
         {
-            for (int i = 0; i < options.Length; i++)
-            {
-                if (options[i] == element)
-                {
-                    return i;
-                }
-            }
+            var index = Array.IndexOf(options, element);
 
-            return 0;
+            return index == -1 ? 0 : index;
         }
     }
 }
