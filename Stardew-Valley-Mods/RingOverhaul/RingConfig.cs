15060cab573d7809c41ef07205807ad932ea54b8
Goldenrevolver <22944589+Goldenrevolver@users.noreply.github.com>
2022-01-16 00:46:42 +0100

    New Mods: Enchantable Scythes and Ring Overhaul

---
+++ Stardew-Valley-Mods/RingOverhaul/RingConfig.cs
@@ -0,0 +1,90 @@
+﻿namespace RingOverhaul
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using StardewModdingAPI;
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    public interface IGenericModConfigMenuApi
+    {
+        void RegisterModConfig(IManifest mod, Action revertToDefault, Action saveToFile);
+
+        void RegisterLabel(IManifest mod, string labelName, string labelDesc);
+
+        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<bool> optionGet, Action<bool> optionSet);
+
+        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<int> optionGet, Action<int> optionSet);
+
+        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<float> optionGet, Action<float> optionSet);
+
+        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<string> optionGet, Action<string> optionSet);
+
+        void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<SButton> optionGet, Action<SButton> optionSet);
+
+        void RegisterClampedOption(IManifest mod, string optionName, string optionDesc, Func<int> optionGet, Action<int> optionSet, int min, int max);
+
+        void RegisterClampedOption(IManifest mod, string optionName, string optionDesc, Func<float> optionGet, Action<float> optionSet, float min, float max);
+
+        void RegisterChoiceOption(IManifest mod, string optionName, string optionDesc, Func<string> optionGet, Action<string> optionSet, string[] choices);
+
+        void RegisterComplexOption(IManifest mod, string optionName, string optionDesc, Func<Vector2, object, object> widgetUpdate, Func<SpriteBatch, Vector2, object, object> widgetDraw, Action<object> onSave);
+
+        void AddParagraph(IManifest mod, Func<string> text);
+    }
+
+    /// <summary>
+    /// Config file for the mod
+    /// </summary>
+    public class RingConfig
+    {
+        public bool CraftableGemRings { get; set; } = true;
+
+        public bool MinorRingCraftingChanges { get; set; } = true;
+
+        public bool OldGlowStoneRingRecipe { get; set; } = false;
+
+        public bool OldIridiumBandRecipe { get; set; } = false;
+
+        public static void VerifyConfigValues(RingOverhaul mod)
+        {
+            try
+            {
+                // CommonFiddleheadFern and ForageSurvivalBurger
+                mod.Helper.Content.InvalidateCache("Data/CraftingRecipes");
+
+                // Tapper days needed changes
+                mod.Helper.Content.InvalidateCache("Data/ObjectInformation");
+            }
+            catch (Exception e)
+            {
+                mod.DebugLog($"Exception when trying to invalidate cache on config change {e}");
+            }
+        }
+
+        [SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1107:CodeMustNotContainMultipleStatementsOnOneLine", Justification = "Reviewed.")]
+        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "Necessary.")]
+        public static void SetUpModConfigMenu(RingConfig config, RingOverhaul mod)
+        {
+            IGenericModConfigMenuApi api = mod.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+
+            if (api == null)
+            {
+                return;
+            }
+
+            var manifest = mod.ModManifest;
+
+            api.RegisterModConfig(manifest, () => config = new RingConfig(), delegate { mod.Helper.WriteConfig(config); VerifyConfigValues(mod); });
+
+            api.RegisterLabel(manifest, "Crafting", null);
+
+            api.RegisterSimpleOption(manifest, "Craftable Gem Rings", null, () => config.CraftableGemRings, (bool val) => config.CraftableGemRings = val);
+            api.RegisterSimpleOption(manifest, "Minor Ring Crafting Changes", null, () => config.MinorRingCraftingChanges, (bool val) => config.MinorRingCraftingChanges = val);
+            api.RegisterSimpleOption(manifest, "Old Glow Stone Ring Recipe", null, () => config.OldGlowStoneRingRecipe, (bool val) => config.OldGlowStoneRingRecipe = val);
+            api.RegisterSimpleOption(manifest, "Old Iridium Band Recipe", null, () => config.OldIridiumBandRecipe, (bool val) => config.OldIridiumBandRecipe = val);
+
+            api.AddParagraph(manifest, () => "Everything else about the mod can currently not be configured. Feedback is welcome.");
+        }
+    }
+}
---
+++ Stardew-Valley-Mods/RingOverhaul/RingOverhaul.cs
@@ -0,0 +1,121 @@
+﻿namespace RingOverhaul
+{
+    using Microsoft.Xna.Framework.Graphics;
+    using StardewModdingAPI;
+    using System;
+    using System.Collections.Generic;
+
+    public class RingOverhaul : Mod, IAssetEditor
+    {
+        public Texture2D ExplorerRingTexture { get; set; }
+
+        public Texture2D BerserkerRingTexture { get; set; }
+
+        public Texture2D PaladinRingTexture { get; set; }
+
+        private RingConfig config;
+
+        public override void Entry(IModHelper helper)
+        {
+            config = Helper.ReadConfig<RingConfig>();
+
+            Helper.Events.GameLoop.GameLaunched += delegate { RingConfig.SetUpModConfigMenu(config, this); };
+
+            ExplorerRingTexture = Helper.Content.Load<Texture2D>($"assets/explorer_ring.png");
+            BerserkerRingTexture = Helper.Content.Load<Texture2D>($"assets/berserker_ring.png");
+            PaladinRingTexture = Helper.Content.Load<Texture2D>($"assets/paladin_ring.png");
+
+            Patcher.PatchAll(this);
+        }
+
+        /// <summary>
+        /// Small helper method to log to the console because I keep forgetting the signature
+        /// </summary>
+        /// <param name="o">the object I want to log as a string</param>
+        public void DebugLog(object o)
+        {
+            Monitor.Log(o == null ? "null" : o.ToString(), LogLevel.Debug);
+        }
+
+        /// <summary>
+        /// Small helper method to log an error to the console because I keep forgetting the signature
+        /// </summary>
+        /// <param name="o">the object I want to log as a string</param>
+        /// <param name="e">an optional error message to log additionally</param>
+        public void ErrorLog(object o, Exception e = null)
+        {
+            string baseMessage = o == null ? "null" : o.ToString();
+
+            string errorMessage = e == null ? string.Empty : $"\n{e.Message}\n{e.StackTrace}";
+
+            Monitor.Log(baseMessage + errorMessage, LogLevel.Error);
+        }
+
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Data/ObjectInformation") || asset.AssetNameEquals("Data/CraftingRecipes");
+        }
+
+        public void Edit<T>(IAssetData asset)
+        {
+            if (asset.AssetNameEquals("Data/ObjectInformation"))
+            {
+                IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
+
+                // Iridium Band
+                var entry = data[527];
+                var fields = entry.Split('/');
+                fields[^1] = "Increases every weapon stat by 10%.";
+                data[527] = string.Join("/", fields);
+            }
+
+            if (asset.AssetNameEquals("Data/CraftingRecipes"))
+            {
+                IDictionary<string, string> data = asset.AsDictionary<string, string>().Data;
+
+                var recipeChanges = new Dictionary<string, Tuple<string, string>>();
+
+                if (!config.OldGlowStoneRingRecipe)
+                {
+                    recipeChanges["Iridium Band"] = new Tuple<string, string>("337 2 529 1 530 1 531 1 532 1 533 1 534 1", "Combat 9");
+                }
+
+                if (!config.OldGlowStoneRingRecipe)
+                {
+                    recipeChanges["Glowstone Ring"] = new Tuple<string, string>("517 1 519 1", "Mining 4");
+                }
+
+                if (config.MinorRingCraftingChanges)
+                {
+                    recipeChanges["Sturdy Ring"] = new Tuple<string, string>("334 2 86 5 338 5", "Combat 1");
+                    recipeChanges["Warrior Ring"] = new Tuple<string, string>("335 5 382 25 84 10", "Combat 4");
+                }
+
+                foreach (var item in recipeChanges)
+                {
+                    var entry = data[item.Key];
+                    var fields = entry.Split('/');
+                    fields[0] = item.Value.Item1;
+                    fields[^1] = item.Value.Item2;
+                    data[item.Key] = string.Join("/", fields);
+                }
+
+                if (!config.OldGlowStoneRingRecipe)
+                {
+                    data.Add("Glow Ring", "516 1 768 5/Home/517/false/Mining 4");
+                    data.Add("Magnet Ring", "518 1 769 5/Home/519/false/Mining 4");
+                }
+
+                if (config.CraftableGemRings)
+                {
+                    data.Add("Amethyst Ring", "66 1 336 1/Home/529/false/Combat 2");
+                    data.Add("Topaz Ring", "68 1 336 1/Home/530/false/Combat 3");
+                    data.Add("Aquamarine Ring", "62 1 336 1/Home/531/false/Combat 4");
+                    data.Add("Jade Ring", "70 1 336 1/Home/532/false/Combat 6");
+                    data.Add("Emerald Ring", "60 1 336 1/Home/533/false/Combat 7");
+                    data.Add("Ruby Ring", "64 1 336 1/Home/534/false/Combat 8");
+                }
+            }
+        }
+    }
+}
